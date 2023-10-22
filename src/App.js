37bab6c404e7baeb5ff9d348fc8f4101ab690158
nfluk <nickylukic@gmail.com>
2023-10-22 11:28:13 +0200

    added particles package for animated background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Nav from './components/Nav/Nav';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition /> */}
+      <ParticlesBg type="cobweb" bg={true} color="#9400d3" />
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Rank() {
+  return (
+    <div>
+      <div className="white f3">
+        <p>Your current Rank is ...</p>
+      </div>
+      <div className="white f1">
+        <p>#1</p>
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
