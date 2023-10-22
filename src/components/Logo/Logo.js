fefa964d1a27d45aaf85a24105ff3627e097ff7f
nfluk <nickylukic@gmail.com>
2023-10-22 09:44:07 +0200

    added logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './logo.css';
+import logo from './face.png';
 
 function Logo() {
   return (
-    <div>
-      <p>Logo</p>
+    <div className="ma4 mt0 flex justify-center">
+      <Tilt className="tilt pa4 shadow-2 br-100">
+        <div
+          style={{
+            height: '150px',
+            width: '150px',
+            padding: '2.5rem',
+          }}
+        >
+          <img src={logo} alt="logo of a face recognition scanner" />
+        </div>
+      </Tilt>
     </div>
   );
 }
